bfeb25eb5fb31b6914e01135815aac6822b37c25
SelinaWernike <63050357+SelinaWernike@users.noreply.github.com>
2020-06-16 15:23:47 +0200

    COVID_19 API hinzugefuegt. Summary call funktioniert. Allerdings gibt es Probleme beim Parsen der anderen Antworten, da dort ein nicht bennanter Array verwendet wird.

---
+++ Assets/Scripts/COVID19_API.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Net;
+using System.IO;
+
+
+
+[Serializable]
+public class Response {
+    public GlobalC Global;
+    public Countries[] Countries;
+    public string Date;
+
+
+}
+
+[Serializable]
+public class GlobalC {
+    public int NewConfirmed;
+    public int TotalConfirmed;
+    public int NewDeaths;
+    public int TotalDeaths;
+    public int NewRecovered;
+    public int TotalRecovered;
+}
+[Serializable]
+public class Countries {
+    public string Country;
+    public string CountryCode;
+    public string Slug;
+    public int NewConfirmed;
+    public int TotalConfirmed;
+    public int NewDeaths;
+    public int TotalDeaths;
+    public int NewRecovered;
+    public int TotalRecovered;
+    public string Date;
+
+}
+
+[Serializable]
+public class ShortFullResponse {
+    public ShortResponse[] results;
+}
+
+[Serializable]
+public class ShortResponse {
+    public string Country;
+    public string CountryCode;
+    public float Lat;
+    public float Lon;
+    public int Confirmed;
+    public int Deaths;
+    public int Recovered;
+    public int Active;
+    public string Date;
+}
+
+
+public class COVID19_API : MonoBehaviour
+{
+    private const string URL = "https://api.covid19api.com/";
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+        ShortResponse anfrage1 = AccurateDataRequest("portugal");
+        Debug.Log(anfrage1.Deaths);
+        ShortResponse anfrage2= AccurateDataRequest("portugal", "2020-03-21T13:13:30Z");
+        Debug.Log(anfrage2.Deaths);
+
+        Response anfrage3 = dataRequest();
+        Debug.Log(anfrage3.Date);
+        Debug.Log(anfrage3.Countries[1].NewRecovered);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public ShortResponse AccurateDataRequest(string location, string time) {
+         string webURL = URL + "live/country/" + location +"/status/confirmed/date/" + time;
+          Debug.Log(webURL);
+        WebRequest covidRequest = WebRequest.Create(webURL);
+        covidRequest.Timeout=10000;
+        WebResponse Answer = covidRequest.GetResponse();
+        StreamReader reader = new StreamReader(Answer.GetResponseStream());
+        return JsonUtility.FromJson<ShortFullResponse>(reader.ReadToEnd()).results[0];
+    }
+
+   public ShortResponse AccurateDataRequest(string location) {
+       string webURL = URL + "live/country/" + location +"/status/confirmed";
+        Debug.Log(webURL);
+        WebRequest covidRequest = WebRequest.Create(webURL);
+        covidRequest.Timeout=10000;
+        WebResponse Answer = covidRequest.GetResponse();
+        StreamReader reader = new StreamReader(Answer.GetResponseStream());
+        return JsonUtility.FromJson<ShortFullResponse>(reader.ReadToEnd()).results[0];
+   } 
+
+   public Response dataRequest() {
+       string webURL = URL + "summary";
+       Debug.Log(webURL);
+       WebRequest covidRequest = WebRequest.Create(webURL);
+        covidRequest.Timeout=10000;
+        WebResponse Answer = covidRequest.GetResponse();
+        StreamReader reader = new StreamReader(Answer.GetResponseStream());
+        return JsonUtility.FromJson<Response>(reader.ReadToEnd());
+   }
+}

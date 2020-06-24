2e971e85328c49baeb35224200ede777df2da818
SelinaWernike <63050357+SelinaWernike@users.noreply.github.com>
2020-06-24 09:45:28 +0200

    Working add and delete buttons

---
+++ Assets/Scripts/DeleteBtnEvent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+public class DeleteBtnEvent : MonoBehaviour
+{
+    public GameObject dataMenu;
+    public GameObject addMenu;
+    public void deleteAll() {
+        List<GameObject> deleteList = dataMenu.GetComponent<ScrollButtonControl>().getBtnList();
+        deleteList.ForEach(btn => Destroy(btn));
+        dataMenu.GetComponent<ScrollButtonControl>().setBtnList(new List<GameObject>());
+        dataMenu.GetComponent<ScrollButtonControl>().setApiList(new List<IDataAPI>());
+
+        foreach(Transform child in addMenu.transform) {
+            child.gameObject.SetActive(true);
+        }
+    }
+}
--- Assets/Scripts/ScrollButtonControl.cs
+++ Assets/Scripts/ScrollButtonControl.cs
@@ -7,36 +7,34 @@ using System.Collections.Generic;
 public class ScrollButtonControl : MonoBehaviour
 {
     [SerializeField]
-    private GameObject buttonTemplate;
+    private GameObject parent;
 
     private List<IDataAPI> apiList = new List<IDataAPI>();
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    void buttonSpawn(int i) {
-        GameObject button = Instantiate(buttonTemplate) as GameObject;
-        button.SetActive(true);
-
-        button.GetComponent<ScrollButtonButton>().SetText("Button: " + i);
-        button.transform.SetParent(buttonTemplate.transform.parent,false);
-
-    }
-
+    private List<GameObject> btnList = new List<GameObject>();
+   
     public void addButton(GameObject btn) {
         GameObject button = Instantiate(btn) as GameObject;
         button.SetActive(true);
-        button.transform.SetParent(buttonTemplate.transform.parent,false);
+        button.transform.SetParent(parent.transform,false);
         Destroy(button.GetComponent<ItemDragHandler>());
         apiList.Add(button.GetComponent<IDataAPI>());
+        btnList.Add(button);
         int i = 0;
+    }
+
+    public List<GameObject> getBtnList() {
+        return btnList;
+    }
+
+    public void setBtnList(List<GameObject> newList) {
+        btnList = newList;
+    }
+
+    public List<IDataAPI> getApiList() {
+        return apiList;
+    }
+
+    public void setApiList(List<IDataAPI> newList) {
+        apiList = newList;
     }
 }

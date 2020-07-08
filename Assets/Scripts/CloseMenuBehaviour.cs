396ca03f9db6761bae322d846f9e1f4e801fcd13
Tim Oelkers <tim.oelkers@web.de>
2020-07-08 10:48:38 +0200

    Block events on close screen

---
+++ Assets/Scripts/CloseMenuBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CloseMenuBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject mainScreen;
+
+    public void onCloseButtonClick()
+    {
+        mainScreen.GetComponent<CanvasGroup>().blocksRaycasts = false;
+        Camera.main.GetComponent<PhysicsRaycaster>().eventMask = LayerMask.GetMask("Ignore Raycast");
+        gameObject.SetActive(true);
+    }
+
+    public void onQuitButtonClick()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+    public void onAbortButtonClick()
+    {
+        mainScreen.GetComponent<CanvasGroup>().blocksRaycasts = true;
+        Camera.main.GetComponent<PhysicsRaycaster>().eventMask = LayerMask.GetMask("Default");;
+        gameObject.SetActive(false);
+    }
+}
